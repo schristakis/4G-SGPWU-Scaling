dbafbaf052bceccbbaf8bc96147f8e8269a43ed1
Socratis Christakis <52699801+schristakis@users.noreply.github.com>
2022-06-29 13:53:41 +0300

    Delete oai/oai-deployment-files directory

--- oai/oai-deployment-files/start-spgwc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-/opt/oai-spgwc/bin/spgwc -c /opt/oai-spgwc/etc/spgw_c.conf -o
--- oai/oai-deployment-files/testscr.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-echo starting;
-IP=`nslookup 5g-control  | grep -v "#" | awk "/Address/ {print $2}"`;
-echo ip is $IP;
-
